299470772b0521b99b9a03e90025a4dfa86cf72c
Steve Jensen <docwho2@gmail.com>
2016-12-30 09:46:50 -0700

    clean up

--- src/main/java/com/sst/alexatempurpedic/TempurpedicErgoPremier.java
+++ src/main/java/com/sst/alexatempurpedic/TempurpedicErgoPremier.java
@@ -22,4 +22,25 @@ package com.sst.alexatempurpedic;
  */
 public class TempurpedicErgoPremier {
     
+    
+    
+    /*
+    Convert Hex String from Wire Sharks to byte array to put into
+    UDP packets.
+    */
+    private static byte[] hexStringToByteArray(String hexString) {
+        int length = hexString.length();
+        byte[] buffer = new byte[length / 2];
+        for (int i = 0 ; i < length ; i += 2) {
+            buffer[i / 2] = (byte)((toByte(hexString.charAt(i)) << 4) | toByte(hexString.charAt(i+1)));
+        }
+        return buffer;
+    }  
+    
+    private static int toByte(char c) {
+        if (c >= '0' && c <= '9') return (c - '0');
+        if (c >= 'A' && c <= 'F') return (c - 'A' + 10);
+        if (c >= 'a' && c <= 'f') return (c - 'a' + 10);
+        throw new RuntimeException ("Invalid hex char '" + c + "'");
+    }
 }
